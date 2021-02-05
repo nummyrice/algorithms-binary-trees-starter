cc9350022e4aab0ecdc334df0f4c9b77456d08cb
Derek Jou <derekjou0@gmail.com>
2021-02-05 15:49:16 -0800

    initial commit, wednesday

---
+++ practice/recursive-traversals.js
@@ -0,0 +1,32 @@
+// Implement the TreeNode class here first befoer continuing
+
+class BinaryTreeNode {
+    constructor(){
+
+    }
+}
+
+// The binary tree class is implemented for you, implement the recursive
+// traversals as methods of the BinaryTree class
+class BinaryTree {
+    constructor(head = null){
+        this.head = head;
+    }
+
+    preOrder(head) {
+
+    }
+
+    inOrder(head) {
+
+    }
+
+    postOrder(head) {
+
+    }
+}
+
+module.exports = {
+    BinaryTreeNode,
+    BinaryTree
+}
---
+++ practice/tree-practice.js
@@ -0,0 +1,27 @@
+// Practice problems on trees
+// Import the BinaryTreeNode class if needed to test your functions
+
+const findMin = root = {
+
+};
+
+const getHeight = root = {
+
+};
+ 
+const balancedTree = root = {
+
+};
+
+const constructTree = root = {
+
+};
+
+const mergeTrees = (root1, root2) = {
+
+};
+
+const deleteNode = root = {
+
+};
+
