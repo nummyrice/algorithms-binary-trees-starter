94a229c44fb9784c9281010497ca1237ed2d7cd8
Brady Fukumoto <bradyfukumoto@gmail.com>
2021-05-02 14:44:32 -0700

    Starter

---
+++ binary-search-tree.js
@@ -0,0 +1,66 @@
+// Do not change this
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+
+  constructor() {
+    this.root = null;
+  }
+
+  insert(val, currentNode=this.root) {
+
+    // Fill this in
+
+  }
+
+  search(val) {
+
+    // Fill this in
+
+  }
+
+
+  // Breadth First Traversal - Iterative
+  breadthFirstTraversal() {
+
+    // Fill this in
+
+  }
+
+  // Depth First Traversal - Iterative
+  depthFirstTraversal() {
+
+    // Fill this in
+
+  }
+
+  preOrderTraversal(currentNode=this.root) {
+
+    // Fill this in
+
+  }
+
+
+  inOrderTraversal(currentNode=this.root) {
+
+    // Fill this in
+
+  }
+
+
+  postOrderTraversal(currentNode=this.root) {
+
+    // Fill this in
+
+  }
+
+
+}
+
+module.exports = [BinarySearchTree, TreeNode];
---
+++ tree-practice.js
@@ -0,0 +1,75 @@
+// Practice problems on binary trees
+
+function findMinBST (rootNode) {
+
+  // Fill this in
+
+}
+
+function findMaxBST (rootNode) {
+
+  // Fill this in
+
+}
+
+function findMinBT (rootNode) {
+
+  // Fill this in
+
+}
+
+function findMaxBT (rootNode) {
+
+  // Fill this in
+
+}
+
+function getHeight (rootNode) {
+
+  // Fill this in
+
+}
+
+function countNodes (rootNode) {
+
+  // Fill this in
+
+}
+
+function balancedTree (rootNode) {
+
+  // Fill this in
+
+}
+
+function getParentNode (rootNode, target) {
+
+  // Fill this in
+
+}
+
+function inOrderPredecessor (rootNode, target) {
+
+  // Fill this in
+
+}
+
+
+function deleteNodeBST (rootNode, target) {
+
+  // Fill this in
+
+}
+
+module.exports = [
+    findMinBST,
+    findMaxBST,
+    findMinBT,
+    findMaxBT,
+    getHeight,
+    countNodes,
+    balancedTree,
+    getParentNode,
+    inOrderPredecessor,
+    deleteNodeBST
+]
