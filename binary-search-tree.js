8e950217b46be1024e5ceca54001c85d77ced4b1
iethan-h <ethanharwell34@gmail.com>
2021-08-04 15:07:09 -0500

    solved breadth first search

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,64 +1,135 @@
 // Do not change this
 class TreeNode {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
 }
 
 class BinarySearchTree {
 
-  constructor() {
-    this.root = null;
-  }
+    constructor() {
+        this.root = null;
+    }
 
-  insert(val, currentNode=this.root) {
+    insert(val, currentNode = this.root) {
 
-    // Fill this in
+        // Fill this in
 
-  }
+        if (currentNode === null) {
+            this.root = new TreeNode(val);
+            return;
+        } else {
+            const searchTree = function(currentNode) {
+                if (val < currentNode.val) {
+                    if (currentNode.left === null) {
+                        currentNode.left = new TreeNode(val);
+                        return;
+                    } else if (currentNode.left !== null) {
+                        return searchTree(currentNode.left);
 
-  search(val) {
+                    }
+                } else if (val > currentNode.val) {
+                    if (currentNode.right === null) {
+                        currentNode.right = new TreeNode(val);
+                        return;
+                    } else if (currentNode.right !== null) {
+                        return searchTree(currentNode.right)
+                    }
+                } else {
+                    return null;
+                }
+            }
+            return searchTree(currentNode)
+        }
 
-    // Fill this in
+    }
 
-  }
+    search(val) {
 
-
-  // Breadth First Traversal - Iterative
-  breadthFirstTraversal() {
-
-    // Fill this in
-
-  }
-
-  // Depth First Traversal - Iterative
-  depthFirstTraversal() {
-
-    // Fill this in
-
-  }
-
-  preOrderTraversal(currentNode=this.root) {
-
-    // Fill this in
-
-  }
+        // Fill this in
+        let current = this.root;
+        while (current) {
+            if (val === current.val) {
+                return true;
+            } else if (val < current.val) {
+                current = current.left
+            } else {
+                current = current.right
+            }
+        }
+        return false;
+    }
 
 
-  inOrderTraversal(currentNode=this.root) {
+    // Breadth First Traversal - Iterative
+    breadthFirstTraversal(current = this.root) {
+        // initialize a queue with the root node 
+        let queue = [];
+        queue.push(current);
+        // while the queue is not empty 
+        while (queue.length) {
+            if (queue[0].left) {
+                queue.push(queue[0].left);
+            }
+            if (queue[0].right) {
+                queue.push(queue[0].right);
+            }
+            // print and remove first node in queue
+            console.log(queue[0].val);
+            queue.shift();
+        }
 
-    // Fill this in
 
-  }
+        // if the node has a left node
+        // push the left node on the back of the queue
+        // if the node has a right node
+        // push the right node on the back of the queue
+
+    }
+
+    // Depth First Traversal - Iterative
+    depthFirstTraversal(current = this.root) {
+
+        // Fill this in
+
+    }
+
+    preOrderTraversal(currentNode = this.root) {
+
+        // Fill this in
+        if (currentNode === null) {
+            return;
+        }
+        console.log(currentNode.val);
+        this.preOrderTraversal(currentNode.left);
+        this.preOrderTraversal(currentNode.right);
+    }
 
 
-  postOrderTraversal(currentNode=this.root) {
+    inOrderTraversal(currentNode = this.root) {
 
-    // Fill this in
+        // Fill this in
+        if (currentNode === null) {
+            return;
+        }
+        this.inOrderTraversal(currentNode.left);
+        console.log(currentNode.val);
+        this.inOrderTraversal(currentNode.right);
+    }
 
-  }
+
+    postOrderTraversal(currentNode = this.root) {
+
+        // Fill this in
+        if (currentNode === null) {
+            return;
+        }
+        this.postOrderTraversal(currentNode.left);
+        this.postOrderTraversal(currentNode.right);
+        console.log(currentNode.val);
+    }
 
 
 }
