1e4a3db9471828f549f5f2ab4effd9e6548c7c70
iethan-h <ethanharwell34@gmail.com>
2021-08-04 16:42:31 -0500

    got to find minimum BT

--- tree-practice.js
+++ tree-practice.js
@@ -1,63 +1,83 @@
 // Practice problems on binary trees
 
-function findMinBST (rootNode) {
 
-  // Fill this in
+function findMinBST(rootNode) {
+
+    while (rootNode.left !== null) {
+        rootNode = rootNode.left;
+    }
+    return rootNode.val;
+}
+
+function findMaxBST(rootNode) {
+
+    // Fill this in
+    while (rootNode.right !== null) {
+        rootNode = rootNode.right;
+    }
+    return rootNode.val;
+}
+
+function findMinBT(rootNode, min = rootNode.val) {
+    if (rootNode === null) {
+        return;
+    }
+    if (rootNode.val < min) {
+        min = rootNode.val;
+
+    }
+    if (rootNode.right) {
+        rootNode = rootNode.right;
+        findMinBT(rootNode, min)
+    }
+    if (rootNode.left) {
+        rootNode = rootNode.left;
+        findMinBT(rootNode, min)
+    }
+
+    return min;
+}
+
+function findMaxBT(rootNode) {
+
+    // Fill this in
 
 }
 
-function findMaxBST (rootNode) {
+function getHeight(rootNode) {
 
-  // Fill this in
-
-}
-
-function findMinBT (rootNode) {
-
-  // Fill this in
+    // Fill this in
 
 }
 
-function findMaxBT (rootNode) {
+function countNodes(rootNode) {
 
-  // Fill this in
-
-}
-
-function getHeight (rootNode) {
-
-  // Fill this in
+    // Fill this in
 
 }
 
-function countNodes (rootNode) {
+function balancedTree(rootNode) {
 
-  // Fill this in
-
-}
-
-function balancedTree (rootNode) {
-
-  // Fill this in
+    // Fill this in
 
 }
 
-function getParentNode (rootNode, target) {
+function getParentNode(rootNode, target) {
 
-  // Fill this in
+    // Fill this in
 
 }
 
-function inOrderPredecessor (rootNode, target) {
+function inOrderPredecessor(rootNode, target) {
 
-  // Fill this in
+    // Fill this in
 
 }
 
 
-function deleteNodeBST (rootNode, target) {
+function deleteNodeBST(rootNode, target) {
 
-  // Fill this in
+    // Fill this in
 
 }
 
